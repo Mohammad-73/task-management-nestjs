fa214aa16f9018ea43e82573f1a84430a91baa70
Mohammad-73 <m.khalili7310@gmail.com>
2025-04-02 17:24:41 +0330

    Implementation Create new project service

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,7 @@
-export class CreateProjectDto {}
+import ProjectStatusEnum from '../enums/project-status-enum';
+
+export class CreateProjectDto {
+  name: string;
+
+  status: ProjectStatusEnum;
+}
