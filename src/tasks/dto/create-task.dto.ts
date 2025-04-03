ff10d96355bc882a60b71653cb0e46dcfc160a41
Mohammad-73 <m.khalili7310@gmail.com>
2025-04-03 14:16:21 +0330

    validation for create and update task

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,24 @@
-export class CreateTaskDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import TaskStatusEnum from '../enums/task-status-enum';
+
+export class CreateTaskDto {
+  @IsString()
+  @MinLength(3)
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @MinLength(10)
+  @IsOptional()
+  description: string;
+
+  @IsEnum(TaskStatusEnum)
+  @IsOptional()
+  status: TaskStatusEnum;
+}
