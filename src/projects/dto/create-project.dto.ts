625aa12c0a9f029d82c6e8a19e50a2733cb0d71c
Mohammad-73 <m.khalili7310@gmail.com>
2025-04-03 14:02:08 +0330

    Set validation for creating project. update project follow the create project dto

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,7 +1,11 @@
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import ProjectStatusEnum from '../enums/project-status-enum';
 
 export class CreateProjectDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
 
+  @IsEnum(ProjectStatusEnum)
   status: ProjectStatusEnum;
 }
